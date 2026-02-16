oracle/graal

Replace custom AArch64StringUTF16CompressOp with AArch64EncodeArrayOp.

compiler/src/jdk.graal.compiler/src/jdk/graal/compiler/lir/aarch64/AArch64EncodeArrayOp.java
@@ -24,11 +24,11 @@
  */
 package jdk.graal.compiler.lir.aarch64;
 
-import static jdk.vm.ci.code.ValueUtil.asRegister;
 import static jdk.graal.compiler.asm.aarch64.AArch64ASIMDAssembler.ASIMDSize.FullReg;
 import static jdk.graal.compiler.asm.aarch64.AArch64Address.createBaseRegisterOnlyAddress;
 import static jdk.graal.compiler.asm.aarch64.AArch64Address.createImmediateAddress;
 import static jdk.graal.compiler.lir.LIRInstruction.OperandFlag.REG;
+import static jdk.vm.ci.code.ValueUtil.asRegister;
 
 import jdk.graal.compiler.asm.Label;
 import jdk.graal.compiler.asm.aarch64.AArch64ASIMDAssembler.ASIMDInstruction;
@@ -38,13 +38,13 @@ import jdk.graal.compiler.asm.aarch64.AArch64Address.AddressingMode;
 import jdk.graal.compiler.asm.aarch64.AArch64Assembler.ConditionFlag;
 import jdk.graal.compiler.asm.aarch64.AArch64Assembler.PrefetchMode;
 import jdk.graal.compiler.asm.aarch64.AArch64MacroAssembler;
-import jdk.graal.compiler.lir.asm.CompilationResultBuilder;
+import jdk.graal.compiler.debug.GraalError;
 import jdk.graal.compiler.lir.LIRInstructionClass;
 import jdk.graal.compiler.lir.Opcode;
 import jdk.graal.compiler.lir.SyncPort;
+import jdk.graal.compiler.lir.asm.CompilationResultBuilder;
 import jdk.graal.compiler.lir.gen.LIRGeneratorTool;
 import jdk.graal.compiler.lir.gen.LIRGeneratorTool.CharsetName;
-
 import jdk.vm.ci.aarch64.AArch64Kind;
 import jdk.vm.ci.code.Register;
 import jdk.vm.ci.meta.AllocatableValue;
@@ -81,31 +81,46 @@ public final class AArch64EncodeArrayOp extends AArch64ComplexVectorOp {
         this.srcValue = tool.newVariable(src.getValueKind());
         this.dstValue = tool.newVariable(dst.getValueKind());
 
-        this.vectorTempValue = allocateConsecutiveVectorRegisters(tool, charset == CharsetName.ASCII ? 7 : 6);
-
+        this.vectorTempValue = allocateVectorRegisters(tool, charset);
         this.charset = charset;
-        assert charset == CharsetName.ASCII || charset == CharsetName.ISO_8859_1 : charset;
+    }
+
+    public static Value[] allocateVectorRegisters(LIRGeneratorTool tool, CharsetName charset) {
+        switch (charset) {
+            case ASCII -> {
+                return allocateConsecutiveVectorRegisters(tool, 7);
+            }
+            case ISO_8859_1 -> {
+                return allocateConsecutiveVectorRegisters(tool, 6);
+            }
+            default -> throw GraalError.shouldNotReachHereUnexpectedValue(charset); // ExcludeFromJacocoGeneratedReport
+        }
     }
 
     @Override
     protected void emitCode(CompilationResultBuilder crb, AArch64MacroAssembler masm) {
-        AArch64Move.move(AArch64Kind.QWORD, crb, masm, srcValue, originSrcValue);
-        AArch64Move.move(AArch64Kind.QWORD, crb, masm, dstValue, originDstValue);
-
-        boolean ascii = charset == CharsetName.ASCII;
-
         Register src = asRegister(srcValue);
         Register dst = asRegister(dstValue);
         Register len = asRegister(lenValue);
         Register res = asRegister(resultValue);
 
-        Register vtmp0 = asRegister(vectorTempValue[0]);
-        Register vtmp1 = asRegister(vectorTempValue[1]);
-        Register vtmp2 = asRegister(vectorTempValue[2]);
-        Register vtmp3 = asRegister(vectorTempValue[3]);
-        Register vlo0 = asRegister(vectorTempValue[4]);
-        Register vlo1 = asRegister(vectorTempValue[5]);
-        Register vmask = ascii ? asRegister(vectorTempValue[6]) : null;
+        AArch64Move.move(AArch64Kind.QWORD, crb, masm, srcValue, originSrcValue);
+        AArch64Move.move(AArch64Kind.QWORD, crb, masm, dstValue, originDstValue);
+
+        emitEncodeArrayOp(masm, res, src, dst, len, vectorTempValue, charset);
+    }
+
+    public static void emitEncodeArrayOp(AArch64MacroAssembler masm, Register res, Register src, Register dst, Register len, Value[] vectorRegisters, CharsetName charset) {
+        GraalError.guarantee(charset == CharsetName.ASCII || charset == CharsetName.ISO_8859_1, charset.toString());
+        boolean ascii = charset == CharsetName.ASCII;
+
+        Register vtmp0 = asRegister(vectorRegisters[0]);
+        Register vtmp1 = asRegister(vectorRegisters[1]);
+        Register vtmp2 = asRegister(vectorRegisters[2]);
+        Register vtmp3 = asRegister(vectorRegisters[3]);
+        Register vlo0 = asRegister(vectorRegisters[4]);
+        Register vlo1 = asRegister(vectorRegisters[5]);
+        Register vmask = ascii ? asRegister(vectorRegisters[6]) : null;
 
         Register cnt = res;
 

bulasevich/graalvm-community-jdk21u

(cherry picked from commit af703de350d9a3efc98ab6e2719f286b5795a35c)

compiler/src/jdk.internal.vm.compiler/src/org/graalvm/compiler/lir/aarch64/AArch64EncodeArrayOp.java
@@ -38,6 +38,7 @@ import org.graalvm.compiler.asm.aarch64.AArch64Address.AddressingMode;
 import org.graalvm.compiler.asm.aarch64.AArch64Assembler.ConditionFlag;
 import org.graalvm.compiler.asm.aarch64.AArch64Assembler.PrefetchMode;
 import org.graalvm.compiler.asm.aarch64.AArch64MacroAssembler;
+import org.graalvm.compiler.debug.GraalError;
 import org.graalvm.compiler.lir.LIRInstructionClass;
 import org.graalvm.compiler.lir.Opcode;
 import org.graalvm.compiler.lir.SyncPort;
@@ -81,31 +82,47 @@ public final class AArch64EncodeArrayOp extends AArch64ComplexVectorOp {
         this.srcValue = tool.newVariable(src.getValueKind());
         this.dstValue = tool.newVariable(dst.getValueKind());
 
-        this.vectorTempValue = allocateConsecutiveVectorRegisters(tool, charset == CharsetName.ASCII ? 7 : 6);
-
+        this.vectorTempValue = allocateVectorRegisters(tool, charset);
         this.charset = charset;
         assert charset == CharsetName.ASCII || charset == CharsetName.ISO_8859_1;
     }
 
+    public static Value[] allocateVectorRegisters(LIRGeneratorTool tool, CharsetName charset) {
+        switch (charset) {
+            case ASCII -> {
+                return allocateConsecutiveVectorRegisters(tool, 7);
+            }
+            case ISO_8859_1 -> {
+                return allocateConsecutiveVectorRegisters(tool, 6);
+            }
+            default -> throw GraalError.shouldNotReachHereUnexpectedValue(charset); // ExcludeFromJacocoGeneratedReport
+        }
+    }
+
     @Override
     protected void emitCode(CompilationResultBuilder crb, AArch64MacroAssembler masm) {
-        AArch64Move.move(AArch64Kind.QWORD, crb, masm, srcValue, originSrcValue);
-        AArch64Move.move(AArch64Kind.QWORD, crb, masm, dstValue, originDstValue);
-
-        boolean ascii = charset == CharsetName.ASCII;
-
         Register src = asRegister(srcValue);
         Register dst = asRegister(dstValue);
         Register len = asRegister(lenValue);
         Register res = asRegister(resultValue);
 
-        Register vtmp0 = asRegister(vectorTempValue[0]);
-        Register vtmp1 = asRegister(vectorTempValue[1]);
-        Register vtmp2 = asRegister(vectorTempValue[2]);
-        Register vtmp3 = asRegister(vectorTempValue[3]);
-        Register vlo0 = asRegister(vectorTempValue[4]);
-        Register vlo1 = asRegister(vectorTempValue[5]);
-        Register vmask = ascii ? asRegister(vectorTempValue[6]) : null;
+        AArch64Move.move(AArch64Kind.QWORD, crb, masm, srcValue, originSrcValue);
+        AArch64Move.move(AArch64Kind.QWORD, crb, masm, dstValue, originDstValue);
+
+        emitEncodeArrayOp(masm, res, src, dst, len, vectorTempValue, charset);
+    }
+
+    public static void emitEncodeArrayOp(AArch64MacroAssembler masm, Register res, Register src, Register dst, Register len, Value[] vectorRegisters, CharsetName charset) {
+        GraalError.guarantee(charset == CharsetName.ASCII || charset == CharsetName.ISO_8859_1, charset.toString());
+        boolean ascii = charset == CharsetName.ASCII;
+
+        Register vtmp0 = asRegister(vectorRegisters[0]);
+        Register vtmp1 = asRegister(vectorRegisters[1]);
+        Register vtmp2 = asRegister(vectorRegisters[2]);
+        Register vtmp3 = asRegister(vectorRegisters[3]);
+        Register vlo0 = asRegister(vectorRegisters[4]);
+        Register vlo1 = asRegister(vectorRegisters[5]);
+        Register vmask = ascii ? asRegister(vectorRegisters[6]) : null;
 
         Register cnt = res;
 