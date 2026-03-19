oracle/graal

Improve crash log.

substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/HeapChunkLogging.java
@@ -72,7 +72,7 @@ class HeapChunkLogging {
         }
     }
 
-    private static void logChunk(Log log, HeapChunk.Header<?> chunk, Pointer bottom, Pointer top, Pointer end, boolean isAligned, String shortSpaceName, boolean isToSpace) {
+    public static void logChunk(Log log, HeapChunk.Header<?> chunk, Pointer bottom, Pointer top, Pointer end, boolean isAligned, String shortSpaceName, boolean isToSpace) {
         log.string("|").zhex(chunk).string("|").zhex(bottom).string(", ").zhex(top).string(", ").zhex(end);
         log.string("|");
         if (top.isNonNull()) {

bulasevich/graalvm-community-jdk21u

substratevm/src/com.oracle.svm.core.genscavenge/src/com/oracle/svm/core/genscavenge/HeapChunkLogging.java
@@ -76,7 +76,7 @@ class HeapChunkLogging {
         }
     }
 
-    private static void logChunk(Log log, HeapChunk.Header<?> chunk, Pointer bottom, Pointer top, Pointer end, boolean isAligned, String shortSpaceName, boolean isFromSpace) {
+    public static void logChunk(Log log, HeapChunk.Header<?> chunk, Pointer bottom, Pointer top, Pointer end, boolean isAligned, String shortSpaceName, boolean isFromSpace) {
         UnsignedWord used = top.subtract(bottom);
         UnsignedWord capacity = end.subtract(bottom);
         UnsignedWord usedPercent = used.multiply(100).unsignedDivide(capacity);